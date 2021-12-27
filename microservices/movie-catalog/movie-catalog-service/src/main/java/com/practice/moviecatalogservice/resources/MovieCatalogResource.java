f839434e8eaa6add1deb2e4ac692299007a34948
Darshan Malaviya <darshan036547@gmail.com>
2021-12-26 22:05:15 -0700

    Initial service skeleton

---
+++ microservices/movie-catalog/movie-catalog-service/src/main/java/com/practice/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,21 @@
+package com.practice.moviecatalogservice.resources;
+
+import com.practice.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("3 Idiots", "Hilarious Movie", 8)
+        );
+    }
+}
